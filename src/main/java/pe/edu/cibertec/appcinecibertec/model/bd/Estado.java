aa0e8700295c1409b7ce09fe9004049cb0fc03cc
ajSL17 <zero_gmz@outlook.com>
2023-10-22 10:56:40 -0500

    add metodos crud

---
+++ src/main/java/pe/edu/cibertec/appcinecibertec/model/bd/Estado.java
@@ -0,0 +1,15 @@
+package pe.edu.cibertec.appcinecibertec.model.bd;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "estado")
+public class Estado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idestado;
+    @Column(name = "desestado")
+    private String desestado;
+}
---
+++ src/main/java/pe/edu/cibertec/appcinecibertec/service/EstadoService.java
@@ -0,0 +1,27 @@
+package pe.edu.cibertec.appcinecibertec.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.appcinecibertec.model.bd.Estado;
+import pe.edu.cibertec.appcinecibertec.repository.EstadoRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EstadoService {
+
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> listarEstados() {
+        return estadoRepository.findAll();
+    }
+
+    public boolean registrarEstado(Estado estado){
+        return estadoRepository.save(estado) != null;
+    }
+
+    public void eliminarEstado(Integer idestado){
+         estadoRepository.deleteById(idestado);
+    }
+}
